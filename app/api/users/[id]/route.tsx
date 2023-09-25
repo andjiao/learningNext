07813040d1cfe5bd473494ef23629f2327b5c212
andreajiao <andreajiao@gmail.com>
2023-09-25 09:16:57 +0200

    updating object

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -10,3 +10,18 @@ export function GET(
 
         return NextResponse.json({id:1, name: "andrea"})
     }
+
+    export async function PUT(
+        req:NextRequest, 
+        {params}: { params: {id: number}})
+        {
+            const body = await req.json()
+            if(!body.name)
+            return NextResponse.json({error:"name is required"},{status:400})
+
+            if(params.id > 10)
+            return NextResponse.json({error: "user not found"}, {status:404})
+
+            return NextResponse.json({id:1, name:body.name})
+
+        }
