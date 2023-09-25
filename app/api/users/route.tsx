629f3d747a9b817a5c1f82a5807c72626fbeb5ad
andreajiao <andreajiao@gmail.com>
2023-09-25 09:04:35 +0200

    object created

--- app/api/users/route.tsx
+++ app/api/users/route.tsx
@@ -5,5 +5,11 @@ export function GET(req: NextRequest){
         {id:1, name: "Andreea"},
         {id:2, name: "Jonathan"},
     ])
+}
 
+export async function POST(req: NextRequest){
+const body = await req.json()
+if(!body.name)
+return NextResponse.json({error: "Name is required"}, {status:400})
+return NextResponse.json({id:1, name: body.name}, {status:201}) // 201 mean object created
 }
