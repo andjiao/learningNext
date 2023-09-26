1c02247512bf9cbbe5ada18e371f1271cfca6e52
andreajiao <andreajiao@gmail.com>
2023-09-26 09:05:47 +0200

    creating data

--- app/api/users/route.tsx
+++ app/api/users/route.tsx
@@ -13,5 +13,20 @@ const body = await req.json()
 const validation = schema.safeParse(body)
 if(!validation.success)
 return NextResponse.json(validation.error.errors, {status:400})
-return NextResponse.json({id:1, name: body.name}, {status:201}) // 201 mean object created
+
+const user = await prisma.user.findUnique({
+    where: {
+        email: body.email
+    }
+})
+
+if(user)
+return NextResponse.json({error: "user already exist"}, {status: 400})
+const newUser = await prisma.user.create({
+    data: {
+        name: body.name,
+        email: body.email
+    }
+})
+return NextResponse.json(newUser, {status:201}) // 201 mean object created
 }
