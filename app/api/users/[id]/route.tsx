3a1698511f4c7d4d9d7c9a0c70ce6f06e2ae2a2e
andreajiao <andreajiao@gmail.com>
2023-09-26 09:18:28 +0200

    updating data

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -21,17 +21,32 @@ export async function GET(
 
     export async function PUT(
         req:NextRequest, 
-        {params}: { params: {id: number}})
+        {params}: { params: {id: string}})
         {
             const body = await req.json()
             const validation = schema.safeParse(body)
             if(!validation.success)
               return NextResponse.json(validation.error.errors,{status:400})
+            
+              const user = await prisma.user.findUnique({
+                where:{
+                  id: parseInt(params.id)
+                }
+              })
 
-            if(params.id > 10)
+            if(!user)
               return NextResponse.json({error: "user not found"}, {status:404})
 
-            return NextResponse.json({id:1, name:body.name})
+              const updatedUser = await prisma.user.update({
+                where:{id:user.id},
+                data: {
+                  name:body.name,
+                  email: body.email
+                }
+                 
+                })
+
+            return NextResponse.json(updatedUser)
 
         }
     
